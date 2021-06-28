d1389fc8ee860535ea6af4b018be5d318ebcc2e7
Craig Pierce <pierca1@students.fscj.edu>
2021-06-27 20:06:12 -0700

    first netmiko_lab commit

---
+++ netmiko_ssh.py
@@ -0,0 +1,15 @@
+#Lsb 7 Netmiko lab_ss
+
+from netmiko import ConnectHandler
+from getpass import getpass
+
+USERNAME = input("Please enter SSh username: ")
+PASS = getpass("Please enter your SSH password: ")
+
+device = {'ip' : '192.168.220.11', 'username' : USERNAME, 'password' : PASS, 'device_type' : 'cisco_ios'}
+
+
+c = ConnectHandler(**device)
+
+output = c.send_command('show run')
+print(output)
